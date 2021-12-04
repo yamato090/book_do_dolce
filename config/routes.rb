59f3e8a8a5349099f8277b4ba2c8517aaa3e2936
yamato090 <tomoki.y.0924@icloud.com>
2021-12-04 11:14:16 +0000

    add コントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'recipes/index'
+  get 'recipes/new'
+  get 'recipes/show'
+  get 'recipes/edit'
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
+  get 'users/unsubscribe'
+  get 'homes/top'
+  get 'homes/about'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
