67f9c624a64e6db4e3ed2853baebf4e4ac96f00c
yamato090 <tomoki.y.0924@icloud.com>
2021-12-04 12:07:06 +0000

    add ユーザーサインアップ完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  get 'recipes/index'
-  get 'recipes/new'
-  get 'recipes/show'
-  get 'recipes/edit'
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'users/unsubscribe'
-  get 'homes/top'
-  get 'homes/about'
   devise_for :users
+  root to: 'homes#top'
+
+  get "/home/about" => "homes#about"
+  get 'users/unsubscribe' => 'users/unsubscribe'
+  resources :users
+  resources :recipes
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
