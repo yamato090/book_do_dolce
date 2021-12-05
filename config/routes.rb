e4cdef248d3bca22a2993562b872b820c15d4389
yamato090 <tomoki.y.0924@icloud.com>
2021-12-05 05:30:57 +0000

    add ヘッダー　フッター　基本ベース完成

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
 
   get "/home/about" => "homes#about"
-  get 'users/unsubscribe' => 'users/unsubscribe'
+  get 'users/unsubscribe' => 'users#unsubscribe'
   resources :users, except: [:new]
   resources :recipes, only: [:index, :create, :new, :show, :edit, :update, :destroy]
-  
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
