d875926cc5779e70204217a55c196558bfb6d777
yamato090 <tomoki.y.0924@icloud.com>
2021-12-05 08:32:41 +0000

    add　ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
   get "/home/about" => "homes#about"
   get 'users/unsubscribe' => 'users#unsubscribe'
-  resources :users, only: [:index, :create, :show, :edit, :update, :destroy]
+  resources :users, except: [:new]
   resources :recipes, only: [:index, :create, :new, :show, :edit, :update, :destroy]
 end
