6c22a8fd84f94173bda6dda8782d13083bca73e8
yamato090 <tomoki.y.0924@icloud.com>
2021-12-21 13:19:47 +0000

    add 修正

--- spec/requests/homes_spec.rb
+++ spec/requests/homes_spec.rb
@@ -1,33 +1,12 @@
 require 'rails_helper'
 
-# RSpec.describe "Homes", type: :request do
-#   describe "GET /top" do
-#     it "レシピ詳細リンクが存在しているのか" do
-#       get "/homes/top"
-#       expect(response).to have_http_status(:success)
-#     end
-#   end
-  
-#   describe "GET /about" do
-#     it "returns http success" do
-#       get "/homes/about"
-#       expect(response).to have_http_status(:success)
-#     end
-#   end
-# end
-
-# RSpec.describe "Hooms", type: :request do
-#   describe "[Action test]" do
-#     before do
-#       @user = FactoryBot.create(:user, :a)
-#     end
-
-#     context "new" do
-#       it "access by user" do
-#         sign_in @user
-#         get '/posts/new'
-#         expect(response).to be_successful
-#       end
-#     end
-#   end
-# end
+RSpec.describe "Homes", type: :request do
+  describe "GET /top" do
+    context '表示の確認' do
+	    it 'トップ画面(root_path)にレシピの一覧ページへのリンクが表示されているか' do
+	     visit root_path
+	     expect(page).to have_link "", href: recipes_path
+	    end
+    end
+  end
+end
